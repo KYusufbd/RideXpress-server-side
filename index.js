269103841a8cf61ba6a3c6dac4e07592c028f42c
Kazi Yusuf <yfaka001@gmail.com>
2025-03-17 22:17:08 +0600

    Connected to mongodb database

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = 5000;
+require('dotenv').config();
+const dbPassword = process.env.PASSWORD;
+const uri = `mongodb+srv://yfaka001_dev:${dbPassword}@cluster0.tiftb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Testing purpose:
 app.get('/', (req, res) => {
@@ -10,3 +14,27 @@ app.get('/', (req, res) => {
 app.listen(5000, () => {
   console.log(`App is listening on port: ${port}`);
 });
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
